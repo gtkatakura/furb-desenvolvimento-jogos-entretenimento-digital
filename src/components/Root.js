ba2c0d081b8a9100dd2dfdceadc26cb7f0defa4f
gtkatakura <gt.katakura@gmail.com>
2017-08-22 20:43:14 -0300

    feat(webpack): add phaser

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import Game from './Game';
 
-const Root = () => <h1>Hello World!!</h1>;
+const Root = () => {
+  global.game = new Game();
+  return <h1>Hello World!</h1>;
+};
 
 export default Root;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,47 @@
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const phaserModule = path.join(__dirname, '/node_modules/phaser-ce/');
+const phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+const pixi = path.join(phaserModule, 'build/custom/pixi.js');
+const p2 = path.join(phaserModule, 'build/custom/p2.js');
+
 module.exports = {
   devtool: 'inline-source-map',
-  entry: [
-    'babel-polyfill',
-    'react-hot-loader/patch',
-    './src/index',
-  ],
+  entry: {
+    app: [
+      'babel-polyfill',
+      'react-hot-loader/patch',
+      './src/index',
+    ],
+    vendor: ['pixi', 'p2', 'phaser']
+  },
+  resolve: {
+    alias: {
+      pixi,
+      phaser,
+      p2,
+    },
+  },
   output: {
     path: `${__dirname}/dist`,
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   module: {
-    loaders: [{
-      test: /.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-    }],
+    loaders: [
+      { test: /.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
+      { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
+      { test: /p2\.js/, use: ['expose-loader?p2'] },
+    ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js',
+    }),
     new HtmlWebpackPlugin({
       template: `${__dirname}/src/index.html`,
       filename: 'index.html',
